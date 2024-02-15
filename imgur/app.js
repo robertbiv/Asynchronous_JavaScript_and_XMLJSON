ac71460f6ec33d7e407b931cbfe0f224972544f9
b1winner <43081501+b1winner@users.noreply.github.com>
2024-02-14 19:16:25 -0500

    Added Details

--- imgur/app.js
+++ imgur/app.js
@@ -1,4 +1,23 @@
-/*var form = new FormData();
+//Modal
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+var form = new FormData();
 var settings = {
     "url": "https://api.imgur.com/3/gallery/search/viral/all/1?q=cats",
     "method": "GET",
@@ -10,28 +29,11 @@ var settings = {
     "mimeType": "multipart/form-data",
     "contentType": false,
     "data": form
-};*/
-/*var form = new FormData();
-var settings = {
-    "url": "https://api.imgur.com/3/credits",
-    "method": "GET",
-    "timeout": 0,
-    "headers": {
-        "Authorization": "Client-ID 0af8b98e102f5cd"
-    },
-    "processData": false,
-    "mimeType": "multipart/form-data",
-    "contentType": false,
-    "data": form
-};*/
-
-/*$.ajax(settings).done(function (response) {
-    console.log(response);
-});*/
+};
 const url = "imgurCache.json";
 //  .get(URL:string, CALLBACK:function)
 let allData
-$.getJSON(url, (data) => {
+$.getJSON(settings, (data) => {
     console.log(data)
     allData = data
     const container = document.getElementById('accordion');
@@ -59,9 +61,10 @@ $.getJSON(url, (data) => {
                 <h5>${result.title}</h5>
                 <p>Likes: ${result.ups}</p>
                 <p>Views: ${result.views}</p>
+                <p>Comments: ${result.comment_count}</p>
               </div>
               <div class="card-action">
-                <a href="">Comments</a>
+                <p onclick="comments('${result.id}')">Comments</p>
                 <a href=${result.link}>More Details</a>
               </div>
           </div>
@@ -74,52 +77,39 @@ $.getJSON(url, (data) => {
         })
 });
 
+function comments(id) {
+    let modalText = ""
+    let form1 = new FormData();
+    form1.append("access_token", "eyJhbGciOiJSUzI1NiIsImtpZCI6ImYwMmZkODgwOTNmNTQ2Mjg1MDY2YTNmNGQxNGNiMzBhZTZhY2MyM2YifQ.eyJpc3MiOiJodHRwczovL2FjY291bnRzLmdvb2dsZS5jb20iLCJhdWQiOiI4NDEzNTkzNTM5ODgtcml0aWhjNDRhdjVwZGwydTBlZWticWI3NzlvaGg2Ym4uYXBwcy5nb29nbGV1c2VyY29udGVudC5jb20iLCJzdWIiOiIxMDY3OTQ2NzEyMzA5MjA4NTQ0MTUiLCJlbWFpbF92ZXJpZmllZCI6dHJ1ZSwiYXpwIjoiODQxMzU5MzUzOTg4LThiMzVsNWFwZTFuYzdyYzR0cm9pYWpqbHRwbWI5cXFkLmFwcHMuZ29vZ2xldXNlcmNvbnRlbnQuY29tIiwiZW1haWwiOiJkcGFzdHVzZWtAZ21haWwuY29tIiwiaWF0IjoxNDU1MTQwMDQyLCJleHAiOjE0NTUxNDM2NDIsIm5hbWUiOiJEYW4gUGFzdHVzZWsiLCJwaWN0dXJlIjoiaHR0cHM6Ly9saDQuZ29vZ2xldXNlcmNvbnRlbnQuY29tLy0yMXV3LVVaMmttMC9BQUFBQUFBQUFBSS9BQUFBQUFBQUZsdy9QcjVLdDdVckxaay9zOTYtYy9waG90by5qcGciLCJnaXZlbl9uYW1lIjoiRGFuIiwiZmFtaWx5X25hbWUiOiJQYXN0dXNlayIsImxvY2FsZSI6ImVuIn0.DcCxKOoMHVUBIppiJDBqLMPUoJk_FMv9RAQNl8mh7HNia3iq5jQCNIea0h3B6-akf7vxfp__b4f3N59qnOcmd4zdAYCT7zO5YubHhyeoOXENwueoTbKzexoNmZMPDZUrKqj-4sAqscHuiJmT2Nwwkdu2g8cFxI0-TpQVHAspJJJ5r_oMP45kdeRpwYV2R7Azct-vYs6QxQaQMn6_azm9Va7HckEop4my8wMoCfAbF1gDiB-JNW63QSzBFBK7SMNXOcrkssoGP3zXdfqjIdU1MK3Wx6i8zqEiV1IrI8XglvWnhIO7z0R_8AAG37DruKNg3HtGRgNtt0grE48ELnQ-zA");
 
-/*const apiResult = [{
-    title: "title1",
-    description: "desc1",
-    output: "out1"
-}, {
-    title: "title2",
-    description: "desc2",
-    output: "out2"
-}, {
-    title: "title3",
-    description: "desc3",
-    output: "out3"
-}];*/
+    let settings1 = {
+        "url": "https://api.imgur.com/3/gallery/"+id+"/comments/best",
+        "method": "GET",
+        "timeout": 0,
+        "headers": {
+            "Authorization": "Client-ID 0af8b98e102f5cd"
+        },
+        "processData": false,
+        "mimeType": "multipart/form-data",
+        "contentType": false,
+        "data": form
+    };
 
+    $.getJSON(settings1, (data2) => {
+        console.log(data2);
+        data2.data.forEach((result, idx) => {
+            console.log(result)
+            modalText += result.author + ": " + result.comment + "\n"
+            console.log(modalText)
+            let text = document.getElementById("textBox");
+            text.innerText = modalText
+        })
 
-/*const container = document.getElementById('accordion');
+    });
 
-apiResult.forEach((result, idx) => {
-    // Create card element
-    const card = document.createElement('div');
-    card.classList = 'card-body';
+    // When the user clicks the button, open the modal
+    let text = document.getElementById("textBox");
+    text.innerText = modalText
+    modal.style.display = "block";
 
-    // Construct card content
-    const content = `
-    <div class="card">
-    <div class="card-header" id="heading-${idx}">
-      <h5 class="mb-0">
-        <button class="btn btn-link" data-toggle="collapse" data-target="#collapse-${idx}" aria-expanded="true" aria-controls="collapse-${idx}">
-
-        </button>
-      </h5>
-    </div>
-
-    <div id="collapse-${idx}" class="collapse show" aria-labelledby="heading-${idx}" data-parent="#accordion">
-      <div class="card-body">
-
-        <h5>${result.title}</h5>
-        <p>${result.description}</p>
-        <p>${result.output}</p>
-        ...
-      </div>
-    </div>
-  </div>
-  `;
-
-    // Append newyly created card element to the container
-    container.innerHTML += content;
-})*/
+}
